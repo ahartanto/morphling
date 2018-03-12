59caea50a1a10d32124f5397d748524572ef068d
anthony hartanto <anthony.hartanto@kudo.co.id>
2018-03-12 11:43:49 +0700

    add set maxConn, MaxIdle, MaxOpen

--- morphling.go
+++ morphling.go
@@ -2,23 +2,23 @@ package morphling
 
 import (
 	"database/sql"
+	"time"
 )
 
-var(
+var (
 	MySQLDriver = "mysql"
 )
 
-
 // Morphling is logical database object with main as master physical database
 // and replica as slave database with load balancer
-type Morphling struct {
-	main *sql.DB
+type DB struct {
+	main    *sql.DB
 	replica *sql.DB
 }
 
 // Open opens master and slave database connection
-func Open(driverName, dataSourceMainStr, dataSourceReplicaStr string) (*Morphling ,error) {
-	Morphling := Morphling{}
+func Open(driverName, dataSourceMainStr, dataSourceReplicaStr string) (*DB, error) {
+	Morphling := DB{}
 
 	dbMain, err := sql.Open(driverName, dataSourceMainStr)
 	if err != nil {
@@ -39,7 +39,7 @@ func Open(driverName, dataSourceMainStr, dataSourceReplicaStr string) (*Morphlin
 //
 // It is rare to Close a DB, as the DB handle is meant to be
 // long-lived and shared between many goroutines.
-func (m *Morphling) Close () error {
+func (m *DB) Close() error {
 
 	err := m.main.Close()
 	if err != nil {
@@ -56,7 +56,7 @@ func (m *Morphling) Close () error {
 
 // Ping verifies connection to all database is still alive,
 // establishing a connection if necessary.
-func (m *Morphling) Ping () error{
+func (m *DB) Ping() error {
 	err := m.main.Ping()
 	if err != nil {
 		return err
@@ -73,20 +73,20 @@ func (m *Morphling) Ping () error{
 // QueryRow executes a query that is expected to return at most one row.
 // QueryRow always returns a non-nil value. Errors are deferred until
 // Row's Scan method is called.
-func (m *Morphling) QueryRow (query string, args ...interface{}) *sql.Row{
-	return m.replica.QueryRow(query, args)
+func (m *DB) QueryRow(query string, args ...interface{}) *sql.Row {
+	return m.replica.QueryRow(query, args...)
 }
 
 // Query executes a query that returns rows, typically a SELECT.
 // The args are for any placeholder parameters in the query.
-func (m *Morphling) Query (query string, args ...interface{}) (*sql.Rows, error){
-	return m.replica.Query(query, args)
+func (m *DB) Query(query string, args ...interface{}) (*sql.Rows, error) {
+	return m.replica.Query(query, args...)
 }
 
 // Exec executes a query without returning any rows.
 // The args are for any placeholder parameters in the query.
-func (m *Morphling) Exec (query string, args ...interface{}) (sql.Result, error){
-	return m.main.Exec(query, args)
+func (m *DB) Exec(query string, args ...interface{}) (sql.Result, error) {
+	return m.main.Exec(query, args...)
 }
 
 // Prepare creates a prepared statement for later queries or executions.
@@ -94,12 +94,50 @@ func (m *Morphling) Exec (query string, args ...interface{}) (sql.Result, error)
 // returned statement.
 // The caller must call the statement's Close method
 // when the statement is no longer needed.
-func (m *Morphling) Prepare (query string) (*sql.Stmt, error){
+func (m *DB) Prepare(query string) (*sql.Stmt, error) {
 	return m.main.Prepare(query)
 }
 
 // Begin starts a transaction. The default isolation level is dependent on
 // the driver.
-func (m *Morphling) Begin (query string) (*sql.Tx, error){
+func (m *DB) Begin() (*sql.Tx, error) {
 	return m.main.Begin()
+}
+
+// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
+//
+// Expired connections may be closed lazily before reuse.
+//
+// If d <= 0, connections are reused forever.
+func (m *DB) SetConnMaxLifetime(d time.Duration) {
+	m.main.SetConnMaxLifetime(d)
+	m.replica.SetConnMaxLifetime(d)
+	return
+}
+
+// SetMaxIdleConns sets the maximum number of connections in the idle
+// connection pool.
+//
+// If MaxOpenConns is greater than 0 but less than the new MaxIdleConns
+// then the new MaxIdleConns will be reduced to match the MaxOpenConns limit
+//
+// If n <= 0, no idle connections are retained.
+func (m *DB) SetMaxIdleConns(n int) {
+	m.main.SetMaxIdleConns(n)
+	m.replica.SetMaxIdleConns(n)
+	return
+}
+
+// SetMaxOpenConns sets the maximum number of open connections to the database.
+//
+// If MaxIdleConns is greater than 0 and the new MaxOpenConns is less than
+// MaxIdleConns, then MaxIdleConns will be reduced to match the new
+// MaxOpenConns limit
+//
+// If n <= 0, then there is no limit on the number of open connections.
+// The default is 0 (unlimited).
+func (m *DB) SetMaxOpenConns(n int) {
+	m.main.SetMaxOpenConns(n)
+	m.replica.SetMaxOpenConns(n)
+	return
 }
